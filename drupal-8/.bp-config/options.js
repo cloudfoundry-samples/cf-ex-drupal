e91a4a436a4f174fa741e91ff483db5de2e601b7
Peter Burkholder <peter.burkholder@gsa.gov>
2018-01-22 11:51:37 -0500

    Should be an error from composer

--- drupal-8/.bp-config/options.js
+++ drupal-8/.bp-config/options.js
@@ -1,5 +1,28 @@
 {
-    "ADMIN_EMAIL": "peter.burkholder@gsa.gov",
-    "WEBDIR": "htdocs/web",
-    "LIBDIR": 
-  }
+  "WEB_SERVER": "httpd",
+  "ADMIN_EMAIL": "your-email@example.com",
+  "COMPOSER_INSTALL_OPTIONS": [
+    "--this-should-fail no-dev --optimize-autoloader --no-progress --no-interaction"
+  ],
+  "COMPOSER_VENDOR_DIR": "app/htdocs/vendor",
+  "WEBDIR": "web",
+  "PHP_MODULES": [
+    "fpm",
+    "cli"
+  ],
+  "PHP_VERSION": "{PHP_70_LATEST}",
+  "PHP_EXTENSIONS": [
+    "bz2",
+    "zlib",
+    "curl",
+    "mcrypt",
+    "openssl",
+    "mbstring",
+    "pdo",
+    "pdo_mysql",
+    "apcu"
+  ],
+  "ZEND_EXTENSIONS": [
+    "opcache"
+  ]
+}
